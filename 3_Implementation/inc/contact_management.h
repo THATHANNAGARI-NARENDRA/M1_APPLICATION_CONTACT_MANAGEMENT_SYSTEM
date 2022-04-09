7761a32854dc4e660c0b78b48245efa0cd2158c1
THATHANNAGARI-NARENDRA <101503664+THATHANNAGARI-NARENDRA@users.noreply.github.com>
2022-04-09 16:02:27 +0530

    Add files via upload

---
+++ 3_Implementation/inc/contact_management.h
@@ -0,0 +1,27 @@
+
+/**
+ * @file contact_management.h
+ * @author pavithra
+ * @brief 
+ * @version 0.1
+ * @date 2021-11-23
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#ifndef CONTACTMANAGEMENT_H
+#define CONTACTMANAGEMENT_H
+
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+
+/* Function Prototype */
+void insert(char *a);
+void update(char *a);
+void delet(char *a);
+void display(char *a);
+void search(char *a);
+
+ 
+#endif //#ifndef CONTACTMANAGEMENT_H
